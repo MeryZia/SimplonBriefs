0ac15a08710f449a526c5fd1ebdf52471abe3a18
MeryZia <zianemaryam07@gmail.com>
2022-08-30 09:21:00 +0100

    My project

---
+++ Brief14-STC-Spring/src/test/java/com/example/STCSpringBoot/Brief14StcSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.STCSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Brief14StcSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
